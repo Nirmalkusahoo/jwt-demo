7afbc41774fd7084a4e0c26ef7afcfd02bcf04ce
Nirmal Kumar Sahoo <nirmal.kumar.sahoo@sap.com>
2022-10-13 23:25:43 +0530

    initail commit

---
+++ src/main/java/com/nirmal/jwtdemo/JwtDemoApplication.java
@@ -0,0 +1,13 @@
+package com.nirmal.jwtdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtDemoApplication {
+	//Resource :: https://www.youtube.com/watch?v=KYNR5js2cXE&t=11s
+	public static void main(String[] args) {
+		SpringApplication.run(JwtDemoApplication.class, args);
+	}
+
+}
